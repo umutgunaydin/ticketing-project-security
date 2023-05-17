0c233f0f0247d02d57080f6e24d1890e9a27f14c
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-18 01:36:12 +0300

    initial commit

--- src/main/java/com/company/TicketingProjectSecurityApplication.java
+++ src/main/java/com/company/TicketingProjectSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication //includes @Configuration
+@SpringBootApplication //includes @Configuration//
 public class TicketingProjectSecurityApplication {
 
     public static void main(String[] args) {
