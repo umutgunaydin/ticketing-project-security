51ac25e751e9d79d7e8cdada8c869e455c431028
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-19 13:09:02 +0300

    rememberMe done

--- src/main/java/com/company/config/SecurityConfig.java
+++ src/main/java/com/company/config/SecurityConfig.java
@@ -1,23 +1,20 @@
 package com.company.config;
 
+import com.company.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 public class SecurityConfig {
+
+    private final SecurityService securityService;
+
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 
 // manual user creation ***
 //    @Bean
@@ -62,6 +59,11 @@ public class SecurityConfig {
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds(120)
+                    .key("company")
+                    .userDetailsService(securityService)
                 .and()
                 .build();
     }
